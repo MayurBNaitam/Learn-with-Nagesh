2deb51f59d490727d28f90b6a012e20f74aac686
Mayur  Naitam <112159635+MayurBNaitam@users.noreply.github.com>
2023-04-26 20:23:00 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function updateProgressBar(){
+  const {scrollTop, scrollHeight} = document.documentElement;
+  const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
+  document.querySelector('#progress-bar').style.setProperty('--progress', scrollPercent);
+}
+
+document.addEventListener('scroll', updateProgressBar);
+
+
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+  loader.style.display="none";
+})
+
+
+
