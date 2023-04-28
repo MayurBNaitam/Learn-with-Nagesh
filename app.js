907c2881e87996108e7b7931e62de4a3e88641e4
Mayur  Naitam <112159635+MayurBNaitam@users.noreply.github.com>
2023-04-28 12:33:38 +0530

    Updated Some Javascript

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Scroll Progress Car 
 function updateProgressBar(){
   const {scrollTop, scrollHeight} = document.documentElement;
   const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
@@ -7,7 +8,7 @@ function updateProgressBar(){
 document.addEventListener('scroll', updateProgressBar);
 
 
-
+// Loading Animation
 var loader = document.getElementById("preloader");
 
 window.addEventListener("load", function(){
